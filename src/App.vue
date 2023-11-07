6cc5cfde13c1c8ad1a62a70c7977915b9a1a0f84
Jewel Josef P. Seno <jjpseno@VDX-MBA39.local>
2023-11-07 17:01:12 +0800

    added greeting in header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
 
 <template>
   <RouterView />
+  <section>
+    <h3>Hello, <input type="text" v-model="name"></h3>
+  </section>
+
 </template>
+  <script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import {ref, onMounted, computed, watch } from 'vue'
+  
+  const todos = ref([])
+  //make point to db
+  const name = ref('')
+
+  watch(name, () => {
+    localStorage.setItem('name', name.value)
+  })
+
+  onMounted(() => {
+    name.value = localStorage.getItem('name') || ''
+  })
+
+  </script>
 
 <style scoped>
 </style>
